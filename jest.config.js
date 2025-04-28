11ad3f2de4abcb9489d674abb3228f4310487713
vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>
2025-04-28 18:52:36 +0530

    test: added jest config

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    roots: ['<rootDir>/tests'],
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest'
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1'
+    }
+};
